0309e388ab20018bb4a3737e5c4cb7cdb64a8153
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2024-03-24 18:37:18 +0530

    correct the admin entry point

--- src/components/SignupComponents/LoginFormAdmin/LoginFormAdmin.js
+++ src/components/SignupComponents/LoginFormAdmin/LoginFormAdmin.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputComponent from "../../common/Input";
 import Button from "../../common/Button";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../../../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, onSnapshot, query } from "firebase/firestore";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUser } from "../../../slices/userSlice";
@@ -14,15 +14,38 @@ function LoginFormAdmin() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
-
+    const [data, setData] = useState([]);
     const navigate = useNavigate();
+
+    console.log("data", data)
+
+    //here we get the all podcast data to show the 'filter output'
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            query(collection(db, "admin")),
+            (querySnapshot) => {
+                const userData = [];
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.data());
+                    userData.push({ id: doc.id, ...doc.data() });
+                });
+                setData(userData);
+
+            },
+            (error) => {
+                console.error("Error fetching podcasts:", error);
+            }
+        );
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     const handleLogin = async () => {
         console.log("Handling Login");
         setLoading(true);
-        if (email == process.env.REACT_APP_USERNAME1 && password == process.env.REACT_APP_PASSWORD1 ||
-            email == process.env.REACT_APP_USERNAME2 && password == process.env.REACT_APP_PASSWORD2
-        ) {
+        if (data[0].id == email && data[0].password == password || data[1].id == email && data[1].password == password) {
             console.log("admin")
             navigate("/admin");
         } else {
@@ -30,6 +53,8 @@ function LoginFormAdmin() {
             setLoading(false);
         }
     };
+
+
     return (
         <div className="parent-div-admin-login">
             <Header />
